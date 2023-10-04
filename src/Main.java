cc90bf4e72863d7a90371d32c94524ab5fe529ab
Lucie Houdkova <lucachat@seznam.cz>
2023-10-04 16:33:02 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.net.Inet4Address;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String jmenoPrijmeni = "Jan Janecek"; //Jméno a příjmení prodejce
+        LocalDate datumNarozeni = LocalDate.of(1991, 5,20); //Datum narození prodejce.
+        int pocetSmluv = 100; //Počet dosud sjednaných smluv.
+        double celkoveProdaneMrkve = 10.5;//Celkové množství prodané mrkve v tunách.
+        String mesto = "Praha"; //Název města, kde prodejce sídlí.
+        String spz = "8AT4155"; //Registrační značka vozidla („SPZ“).
+        double spotrebaVozidla = 7.8;//Spotřeba firemního vozidla v litrech na 100 km.
+        String ipAdresa = "192.168.10.40";// IP adresa verze 4 firemního počítače.
+        //Více o formátu IP adresy verze 4 viz například IP adresa verze 4;
+        System.out.println(celkoveProdaneMrkve/pocetSmluv);
+        System.out.println("testcommit");
     }
 }
